3b834bf885e909156a37f8d001347fd9438ef450
Jan Koehnlein <jan.koehnlein@typefox.io>
2018-01-10 17:09:32 +0100

    added missing @injectable
    Signed-off-by: Jan Koehnlein <jan.koehnlein@typefox.io>

--- theia-yang-extension/src/frontend/language/dynamic-commands.ts
+++ theia-yang-extension/src/frontend/language/dynamic-commands.ts
@@ -1,7 +1,15 @@
-import { inject } from 'inversify';
+/*
+ * Copyright (C) 2018 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License") you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { inject, injectable } from 'inversify';
 import { CommandRegistry, MenuModelRegistry, Disposable } from '@theia/core/lib/common';
 import { EDITOR_CONTEXT_MENU, EditorManager, EditorWidget } from '@theia/editor/lib/browser';
 
+@injectable()
 export class ContextMenuCommands {
 
     constructor(@inject(MenuModelRegistry) protected menuRegistry: MenuModelRegistry,
